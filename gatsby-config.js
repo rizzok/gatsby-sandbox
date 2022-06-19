168257529d021d369a042f7c52cb223e6ce0543b
rizzok <kevinrizzo13100@gmail.com>
2022-06-19 17:50:24 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     title: `Gatsby Sandbox`,
     // siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
